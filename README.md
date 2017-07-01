731785cc26e99e851e0170ea04d00744dbd78d5e
davideliason <davethemaker@gmail.com>
2017-07-01 06:45:15 -0700

    add express route where it serves json-formated key-value pair successfully

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 ## July 1, 2017
 ### [David Eliason(http://www.deliason.com)
 
+**Description**
+Tying in react.js with backend with express and also using react-router
+
--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express 		= require('express');
 const path 			= require('path');
 
 const app 			= express();
+
+require('dotenv').config();
+
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get("/",(req,res) => {
-	res.send("hello world");
+
+
+app.get("/api/coffee",(req,res) => {
+    const coffee = {key: 1, text : "coffee"};
+    res.json(coffee);
+    console.log(`Sent ${coffee}`);
 });
 
 app.listen(app.get('port'), () => {

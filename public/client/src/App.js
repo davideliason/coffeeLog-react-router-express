dac0e7e0a230d2d949024632595958a381fe8905
davideliason <davethemaker@gmail.com>
2017-07-01 09:20:00 -0700

    successful functioning Home, Coffee, and Drinks with the latter havind two nested links

--- public/client/src/App.js
+++ public/client/src/App.js
@@ -13,7 +13,6 @@ const App = () => (
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/coffee">Coffee</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
         <li><Link to="/drinks">Drinks</Link></li>
       </ul>
 
@@ -21,7 +20,6 @@ const App = () => (
 
       <Route exact path="/" component={Home}/>
       <Route path="/coffee" component={Coffee}/>
-      <Route path="/topics" component={Topics}/>
       <Route path="/drinks" component={Drinks}/>
     </div>
   </Router>
@@ -35,11 +33,7 @@ const Home = () => (
   </div>
 )
 
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-)
+
 
 const Drinks = ({match}) => (
   <div>
@@ -65,39 +59,5 @@ const Drink = ({ match }) => (
          <h3>{match.params.drinkId}</h3>
       </div>
   )
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props v. State
-        </Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:topicId`} component={Topic}/>
-    <Route exact path={match.url} render={() => (
-      <h3>Please select a topic.</h3>
-    )}/>
-  </div>
-)
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-)
 
 export default App

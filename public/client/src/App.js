d00561fef983ebcf3ef78fd97160c88f8a9accb2
davideliason <davethemaker@gmail.com>
2017-07-02 13:06:26 -0700

    successful state finally i think i was just using improper syntax previously

--- public/client/src/App.js
+++ public/client/src/App.js
@@ -3,12 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      colors : ["blue","red"]
+    }
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React {this.state.colors[0]}</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.

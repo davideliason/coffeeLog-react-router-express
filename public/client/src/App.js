546c671d75c577635524b9a6f22b3257ec3ee218
davideliason <davethemaker@gmail.com>
2017-07-03 14:35:02 -0700

    clear Coffee component, props functioning correctly

--- public/client/src/App.js
+++ public/client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   constructor(props){
     super(props);
@@ -13,15 +14,15 @@ class App extends Component {
     var appStyle = {
       backgroundColor: "blue"
     };
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2 style={appStyle}>Welcome to React {this.state.colors[0]}</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <p>{this.props.name}: i'm a prop</p>
       </div>
     );
   }
--- public/client/src/index.js
+++ public/client/src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App name="hi" />, document.getElementById('root'));
 registerServiceWorker();

1f294644bce23dbdb5654ba3eb7342febc68a2b1
davideliason <davethemaker@gmail.com>
2017-07-03 15:55:06 -0700

    remove unneed text node

--- public/client/src/App.js
+++ public/client/src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
     var appStyle = {
       backgroundColor: this.state.colors[this.state.colors.length-1],
       width: 200,
-      marginLeft: 200,
+      marginLeft: 420,
       border: "1px solid white",
       padding: "0 20 20 20"
     };
@@ -46,7 +46,7 @@ class App extends Component {
           <h2 style={appStyle}>Welcome to React </h2>
         </div>
         
-        <p>{this.props.name}: i'm a prop</p>
+        {/*(<p>{this.props.name}: i'm a prop</p>*/}
            <form onSubmit={this.addColor}>
              <input ref={ (a) => this._inputColor = a }placeholder="update color"></input>
              <button type="submit">update</button>

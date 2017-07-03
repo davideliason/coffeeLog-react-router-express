78270201e725018a262da0f766d6b73be3860e73
davideliason <davethemaker@gmail.com>
2017-07-03 15:35:54 -0700

    add Coffee component with initializing a drinks array in state

--- public/client/src/Coffee.jsx
+++ public/client/src/Coffee.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 class Coffee extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			drinks: [];
+		}
+		this.addDrink = this.addDrink.bind(this);
+	}
 	render(){
 		return(
 				<p>Coffee</p>

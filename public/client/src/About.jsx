843c7dad57e7c1bd3bf88c4259d9dea047ac297b
davideliason <davethemaker@gmail.com>
2017-07-03 16:35:31 -0700

    update inline styling for About component

--- public/client/src/About.jsx
+++ public/client/src/About.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
 class About extends React.Component {
+	
+
 	render(){
+		var aboutStyle = {
+			backgroundColor : "blue",
+			width: 400,
+			height: 400,
+			marginLeft: 200,
+			textAlign: "center",
+			paddingTop: 50
+	    };
+
 		return(
-				<div>
-					<p>About</p>
+				<div style={aboutStyle}>
+					<h2>About</h2>
+					<p>Lots of drinks to choose from</p>
 				</div>
 			);
 	}

b4f1280ed04bcfe5f0a7d640100d8e08e3eb54da
davideliason <davethemaker@gmail.com>
2017-07-03 16:25:48 -0700

    successfully create Route with link drinks pointing to App

--- public/client/src/Parent.jsx
+++ public/client/src/Parent.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 import App from './App.js';
 
 class Parent extends Component{
 	render(){
 		return(
-				<App />
+				<Router>
+					<div>
+						<ul>
+							<li><Link to="/">Drinks</Link></li>
+						</ul>
+						<Route exact path="/" component={App} />
+					</div>
+				</Router>
 			);
 	}
 }
